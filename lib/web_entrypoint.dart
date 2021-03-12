f47682e8ac246e9c28c2ffc26544111135b98520
Erik Rosengren <e@roseng.ren>
2021-03-12 19:32:11 +0100

    web test

--- lib/web_entrypoint.dart
+++ lib/web_entrypoint.dart
@@ -2,7 +2,7 @@
 // Flutter web bootstrap script for package:flingjammer/main.dart.
 
 import 'dart:async';
-import 'dart:ui' as ui;
+// import 'dart:ui' as ui;
 
 import 'package:flingjammer/main.dart' as entrypoint;
 import 'package:flutter/foundation.dart';
@@ -10,7 +10,7 @@ import 'package:flutter/foundation.dart';
 typedef _UnaryFunction = dynamic Function(List<String> args);
 typedef _NullaryFunction = dynamic Function();
 Future<void> main() async {
-  await ui.webOnlyInitializePlatform();
+  // await ui.webOnlyInitializePlatform();
   if (entrypoint.main is _UnaryFunction) {
     return (entrypoint.main as _UnaryFunction)(<String>[]);
   }
