d0da3eea68538ebb2a7db963723f6bf213125011
Erik Rosengren <e@roseng.ren>
2021-03-14 01:10:15 +0100

    align

--- lib/disc.dart
+++ lib/disc.dart
@@ -42,23 +42,23 @@ class Disc extends PositionComponent {
   }
   @override
   void render(Canvas c) {
-    return;
+    // return;
     super.render(c);
     palette.style = PaintingStyle.stroke;
     //size = 100/life;
     // palette.color = palette.color.withOpacity(life);
     var s = size;
     // if (life > 1) {
-    double r = max(20, radius - life * 10);
-    s.x = r;
-    s.y = r;
+    // double r = max(20, radius - life * 10);
+    // s.x = r;
+    // s.y = r;
     // }
     palette.strokeWidth = max(4, 10 - 5 * life);
     //s.x=s.x.clamp(10,100);
     c.drawOval(s.toRect(), palette);
     if (!flying && held)
       c.drawOval(s.toRect(), Palette.white.withAlpha(100).paint);
-    r /= 100;
+    // r /= 100;
     // shape?.scaleX = r;
     // shape?.scaleY = r;
     //   -300;
@@ -67,34 +67,26 @@ class Disc extends PositionComponent {
   @override
   void update(double dt) {
     super.update(dt);
+    if (acc != null)
+      speed = Offset(speed.dx - acc.x / MyGame.frameRate,
+          speed.dy + acc.y / MyGame.frameRate);
+    position.x += speed.dx;
+    position.y += speed.dy;
     shape?.x = position.x - spawnPos.x;
     // MyGame.artboard.x = 0;
     shape?.y = position.y - spawnPos.y / 1.7;
     angle += dt;
-    if (shape?.scaleY < 1) shape.scaleY += 0.01 / pow(shape.scaleY, 2);
+    if (shape?.scaleY < 1)
+      shape.scaleY += 1 / (MyGame.frameRate * pow(shape.scaleY, 2));
     // if (shape?.scaleY > 1) shape?.scaleY -= 0.1;
     // position.rotate(dt, center: Vector2(300, 700));
     // speed.dx -= acc?.x;
     // position.y += acc?.y;
+    collision();
+    // if (speed != null) {
+    // }
+    speed *= 0.99;
     if (flying) {
-      if (acc != null)
-        speed = Offset(speed.dx - acc?.x / 120, speed.dy + acc?.y / 120);
-      final height = window.physicalSize.height / window.devicePixelRatio;
-      if (position.y < 0 || position.y > height) {
-        speed = Offset(speed.dx, -speed.dy);
-        position.y += speed.dy;
-        shape?.scaleY =
-            max(0.1, shape.scaleY - speed.dy.abs() / MyGame.frameRate);
-      }
-      if (position.x < 0 || position.x > height) {
-        speed = Offset(-speed.dx, speed.dy);
-        position.x += speed.dx;
-      }
-      // if (speed != null) {
-      position.x += speed.dx;
-      position.y += speed.dy;
-      // }
-      speed *= 0.99;
       life -= 0.01;
 
       if (life < 0) {
@@ -109,6 +101,21 @@ class Disc extends PositionComponent {
     if (flying) {
       var spawnDist = spawnPos - position;
       // position += spawnDist / (life * 10);
+    }
+  }
+
+  void collision() {
+    final h = window.physicalSize.height / window.devicePixelRatio;
+    Rect size = shape.fillPath.getBounds();
+    if (position.y < size.bottom || position.y > h + size.top) {
+      speed = Offset(speed.dx, -speed.dy);
+      shape?.scaleY = max(0.1, shape.scaleY - speed.dy.abs() / 10);
+      position.y += speed.dy;
+    }
+    final w = window.physicalSize.width / window.devicePixelRatio;
+    if (position.x < 0 || position.x > w) {
+      speed = Offset(-speed.dx, speed.dy);
+      position.x += speed.dx;
     }
   }
 
