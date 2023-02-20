0bb9f69b922b326a33360c161262140adc549a41
nergnezor <e@roseng.ren>
2023-02-20 11:30:00 +0100

    stateMachines: ['State Machine 1'],

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
 
 void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.system,
       home: MyRiveAnimation(),
     ));
 
@@ -12,6 +19,7 @@ class MyRiveAnimation extends StatelessWidget {
       body: Center(
         child: RiveAnimation.asset(
           'nerg.riv',
+          stateMachines: ['State Machine 1'],
           fit: BoxFit.cover,
         ),
       ),
